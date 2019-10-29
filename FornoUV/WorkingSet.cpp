d2cc5b73172c91ee9a290d69f5bda862b7062494
paolopasqua <paolo.pasquali@outlook.it>
2019-10-29 14:13:40 +0100

    Ends gets and sets on WorkingSet (TODO reads)

--- FornoUV/WorkingSet.cpp
+++ FornoUV/WorkingSet.cpp
@@ -10,33 +10,66 @@ WorkingSet* WorkingSet::getInstance() {
 }
 
 WorkingSet::WorkingSet() {
+    _ledGStatus = false;
+    _ledYStatus = false;
+    _ledRStatus = false;
+    _ledUVStatus = false;
 
+    _motorStatus = false;
+    _fanStatus = false;
+
+    readLimitSwitch();
+    readTemperature();
 }
 
 /* DEVICE STATUS */
+void WorkingSet::setLedG(bool status) {
+    _ledGStatus = status;
+}
+void WorkingSet::setLedY(bool status) {
+    _ledYStatus = status;
+}
+void WorkingSet::setLedR(bool status) {
+    _ledRStatus = status;
+}
+void WorkingSet::setLedUV(bool status) {
+    _ledUVStatus = status;
+}
+void WorkingSet::setMotor(bool status) {
+    _motorStatus = status;
+}
+void WorkingSet::setFan(bool status) {
+    _fanStatus = status;
+}
 bool WorkingSet::getLedG() {
-    return false;
+    return _ledGStatus;
 }
 bool WorkingSet::getLedY() {
-    return false;
+    return _ledYStatus;
 }
 bool WorkingSet::getLedR() {
-    return false;
+    return _ledRStatus;
 }
 bool WorkingSet::getLedUV() {
-    return false;
+    return _ledUVStatus;
 }
-bool WorkingSet::getEngine() {
-    return false;
+bool WorkingSet::getMotor() {
+    return _motorStatus;
 }
 bool WorkingSet::getFan() {
-    return false;
+    return _fanStatus;
 }
 
 /* SENSORS DATA */
+void WorkingSet::readLimitSwitch() {
+    //TODO
+}
+void WorkingSet::readTemperature() {
+    //TODO
+}
 bool WorkingSet::getLimitSwitch() {
-    return false;
+    return _limitSwitchStatus;
 }
 float WorkingSet::getTemperature() {
-    return 0.0;
+    return _temperature;
 }
--- FornoUV/WorkingSet.h
+++ FornoUV/WorkingSet.h
@@ -1,6 +1,8 @@
 #ifndef WORKING_SET_H
 #define WORKING_SET_H
 
+#define TEMPERATURE_SENSOR_GPIO 42
+
 class WorkingSet {
     public:
         static WorkingSet* getInstance();
@@ -10,15 +12,34 @@ class WorkingSet {
         bool getLedY();
         bool getLedR();
         bool getLedUV();
-        bool getEngine();
+        bool getMotor();
         bool getFan();
 
         /* SENSORS DATA */
+        void readLimitSwitch();
+        void readTemperature();
         bool getLimitSwitch();
         float getTemperature();
     private:
+        friend class Actuator;
         static WorkingSet* _instance;
+        bool _ledGStatus;
+        bool _ledYStatus;
+        bool _ledRStatus;
+        bool _ledUVStatus;
+        bool _motorStatus;
+        bool _fanStatus;
+        bool _limitSwitchStatus;
+        float _temperature;
         WorkingSet();
+
+        /* DEVICE STATUS */
+        void setLedG(bool status);
+        void setLedY(bool status);
+        void setLedR(bool status);
+        void setLedUV(bool status);
+        void setMotor(bool status);
+        void setFan(bool status);
 };
 
 
